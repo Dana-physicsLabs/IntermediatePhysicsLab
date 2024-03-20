5c323097da536b60845d4a0579323436b879e377
Dana <66744759+Dana-physicsLabs@users.noreply.github.com>
2024-03-20 11:01:14 -0400

    UPDATEDDDDD

--- MATLABexample.m
+++ MATLABexample.m
@@ -1,3 +1,39 @@
+% close all 
+
+%%  Significant Figures Check in 
+
+% What are the significant figures on these functions below? 
+
+a = 1.2*0.00004032; 
+
+b = 100/(58^2);
+
+c = (1/2) * pi;
+
+d = a/c^3;
+
+
+%clear all
+
+%% Standard deviaton
+% https://www.mathworks.com/help/matlab/ref/std.html 
+%Importing data:
+dataTable = readtable('foam_1click_10min_clamp.xlsx');
+times = dataTable{:,1};
+heights = dataTable{:,2};
+
+t_stdev = std(times);
+h_stdev = std(heights);
+
+%data_stdev = std(dataTable); %Why do you think it breaks? Hint: Python
+%also has this issues, especailly if you love Pandas
+all_stev = std(times,heights); %Why does this one work? What does it mean physically?
+
+h_mean = mean(heights);
+
+h_stderror = h_stdev/ sqrt(h_mean);
+%% Matlab Functions 
+
 % Variables are vectors/matricies:
 a = [1 2 3 4 5]
 b = [1;2;3;4;5]
@@ -8,29 +44,26 @@ x = 1:10;
 y = 2*x;
 
 %Basic plotting:
-figure; plot(x,y);
-figure; plot(x,y,'r.');
+figure(1); plot(x,y);
+figure(2); plot(x,y,'r.');
 
 %Functions:
 t = 0:.1:10;
 [y,dy] = kinematic(t,0,20,-9.8);
-figure; errorbar(t,y,dy);
-figure; plot(t,y); hold on;
+figure(3); errorbar(t,y,dy);
+figure(4); plot(t,y); hold on;
 for i = 0:20:200
     plot(t,kinematic(t,i,20,-9.8));
 end
 
-%Importing data:
-dataTable = readtable('foam_1click_10min_clamp.xlsx');
-times = dataTable{:,1};
-heights = dataTable{:,2};
+
 
 %Advanced plotting:
-figure; hist(heights);
+figure(5); hist(heights);
 xlabel('Height (cm)'); ylabel('Number of trials');
-figure; plot(times,heights,'o');
+figure(6); plot(times,heights,'o');
 model = fitlm(times,heights) % See https://www.mathworks.com/help/stats/coefficient-standard-errors-and-confidence-intervals.html
-figure; plot(model)
+figure(7); plot(model)
 
 function [x,dx] = kinematic(time, x0,v0,a)
     x = x0 + (v0*time) + (.5*a*(time.^2));
